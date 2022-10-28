0ba984b8c8f740bbd1d755cc1c5c4d94671d588e
Your Name <you@example.com>
2022-10-28 11:48:38 +0530

    first

---
+++ TempService.Tests/UnitTests.cs
@@ -0,0 +1,28 @@
+namespace TempService.Tests;
+
+public class Tests
+{
+    private Temp? TempCalc { get; set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        TempCalc = new Temp(); 
+    }
+
+    [Test]
+    public void TestCTF()
+    {
+        double temp_in_C = 50.0;
+        double temp_in_F = TempCalc.CTF(temp_in_C);
+        Assert.AreEqual(122,temp_in_F);
+    }
+
+    [Test]
+    public void TestFTC()
+    {
+        double temp_in_F = 52.8;
+        double temp_in_C = TempCalc.FTC(temp_in_F);
+        Assert.AreEqual(11.56, temp_in_C);
+    }
+}
